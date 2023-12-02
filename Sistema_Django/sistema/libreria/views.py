9d1c43c3b907c1d8debd9acabb1a59a3c571a36f
Gabriel Garcia <86282715+Cerbero0109@users.noreply.github.com>
2023-12-02 15:42:04 -0600

    Add files via upload

---
+++ Sistema_Django/sistema/libreria/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Libro
+from .forms import LibroForm
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'index.html')
+def libros(request):
+    libros = Libro.objects.all()
+    return render(request,'libros.html', {'libros': libros})
+def crear(request):
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()       
+        return redirect('libros')
+    return render(request,'crear.html', {'formulario': formulario})
+def editar(request, id):
+    libro = Libro.objects.get(id=id)
+    if request.method == 'POST':
+        formulario = LibroForm(request.POST, request.FILES, instance=libro)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('libros')
+    else:
+        formulario = LibroForm(instance=libro)
+
+    return render(request, 'editar.html', {'formulario': formulario, 'libro': libro})
+def acerca(request):
+    return render(request,'acerca.html')
+def eliminar(request, id):
+    libro = get_object_or_404(Libro, id=id)
+    libro.delete(using=None, keep_parents=False)
+    return redirect('libros')
